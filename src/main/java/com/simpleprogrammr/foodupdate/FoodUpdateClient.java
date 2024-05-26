7f0fd741a1b57605add6294b59a35599bdc7ffd7
Progrmm <michal.pelon@proton.me>
2024-05-26 21:32:17 +0200

    Initial commit

---
+++ src/main/java/com/simpleprogrammr/foodupdate/FoodUpdateClient.java
@@ -0,0 +1,10 @@
+package com.simpleprogrammr.foodupdate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FoodUpdateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
