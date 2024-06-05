624af97f48ef9506b250fd6a96d898c555255f8b
Progrmm <michal.pelon@proton.me>
2024-06-05 22:00:49 +0200

    Adding Building Blocks

--- src/main/java/com/foodupdate/FoodUpdateClient.java
+++ src/main/java/com/foodupdate/FoodUpdateClient.java
@@ -3,7 +3,6 @@ package com.foodupdate;
 import com.foodupdate.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.block.BlockRenderType;
 import net.minecraft.client.render.RenderLayer;
 
 public class FoodUpdateClient implements ClientModInitializer {
