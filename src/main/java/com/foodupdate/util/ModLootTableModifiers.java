58c3c7fe9732a00e8904401c0a083cd050ea11c3
Progrmm <michal.pelon@proton.me>
2024-06-27 13:05:26 +0200

    Adding Items to Suspicious Sand Loot Table. Modifying Armor Effect

--- src/main/java/com/foodupdate/util/ModLootTableModifiers.java
+++ src/main/java/com/foodupdate/util/ModLootTableModifiers.java
@@ -3,12 +3,18 @@ package com.foodupdate.util;
 import com.foodupdate.item.ModItems;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ModLootTableModifiers {
 
@@ -20,6 +26,9 @@ public class ModLootTableModifiers {
 
     private static final Identifier CREEPER_ID =
             new Identifier("minecraft", "entities/creeper");
+
+    private static final Identifier SUS_SAND_ID =
+            new Identifier("minecraft", "archaeology/desert_pyramid");
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
@@ -53,6 +62,20 @@ public class ModLootTableModifiers {
                 tableBuilder.pool(poolBuilder.build());
             }
 
+        });
+
+        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) -> {
+            if (SUS_SAND_ID.equals(id)) {
+                List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
+                entries.add(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
+                entries.add(ItemEntry.builder(ModItems.COAL_BRIQUETTE).build());
+
+                LootPool.Builder pool = LootPool.builder().with(entries);
+
+                return LootTable.builder().pool(pool).build();
+            }
+
+            return null;
         });
 
     }
