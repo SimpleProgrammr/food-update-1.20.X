f79b8471457edc6479cfabee0b826e2b11d8433d
Progrmm <michal.pelon@proton.me>
2024-06-29 00:06:26 +0200

    Adding Tomato Crops And Tomato Seeds Adding Tomato Seeds recipe Adding custom Advancements

--- src/main/java/com/foodupdate/datagen/ModAdvancementsProvider.java
+++ src/main/java/com/foodupdate/datagen/ModAdvancementsProvider.java
@@ -1,8 +1,15 @@
 package com.foodupdate.datagen;
 
+import com.foodupdate.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
 import net.minecraft.advancement.Advancement;
+import net.minecraft.advancement.AdvancementFrame;
+import net.minecraft.advancement.criterion.InventoryChangedCriterion;
+import net.minecraft.advancement.criterion.TickCriterion;
+import net.minecraft.block.Blocks;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 import java.util.function.Consumer;
 
@@ -13,6 +20,31 @@ public class ModAdvancementsProvider extends FabricAdvancementProvider {
 
     @Override
     public void generateAdvancement(Consumer<Advancement> consumer) {
+        Advancement get_mod = Advancement.Builder.create()
+                .display(Blocks.COARSE_DIRT,
+                        Text.translatable("advancement.get_mod_advancement"),
+                        Text.translatable("advancement.get_mod_advancement_description"),
+                        new Identifier("minecraft:textures/gui/advancements/backgrounds/adventure.png"),
+                        AdvancementFrame.TASK,
+                        true,
+                        true,
+                        false)
+                .criterion("spawned", TickCriterion.Conditions.createTick())
+                .build(consumer, "foodupdate/get_mod");
+
+        Advancement get_tomato = Advancement.Builder.create()
+                .display(ModItems.TOMATO,
+                        Text.translatable("advancement.get_tomato_advancement"),
+                        Text.translatable("advancement.get_tomato_advancement_description"),
+                        new Identifier("minecraft:textures/gui/advancements/backgrounds/adventure.png"),
+                        AdvancementFrame.TASK,
+                        true,
+                        true,
+                        false)
+                .parent(get_mod)
+                .criterion("get_tomato", InventoryChangedCriterion.Conditions.items(ModItems.TOMATO))
+                .build(consumer, "foodupdate/get_tomato");
+
 
     }
 
